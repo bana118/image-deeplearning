a27d16df7c68accf48dfa917ccbd8def4b9213ff
baba-titech <bana.titech@gmail.com>
2018-11-14 20:03:28 +0900

    first commit

---
+++ unity/fbx view/Assets/Screenshot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class Screenshot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ScreenCapture.CaptureScreenshot("test.png");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
